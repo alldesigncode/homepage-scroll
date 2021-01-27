1fa8a20dc4f231c3fa1e2b2e0d9a1f441b54665d
Beka Maisuradze <bekamais@Bekas-MacBook-Pro.local>
2021-01-27 21:53:31 +0400

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-homepage-scroll';
+export class AppComponent implements OnInit, AfterViewInit {
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    gsap.to('.right, .nav-list, .bottom', {
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: '.home',
+        scrub: true,
+        markers: true,
+        start: '60% 60%', // trigger element start, scroller start
+        end: '70% 30%' // trigger element end, scroller end
+      }
+    })
+  }
 }
